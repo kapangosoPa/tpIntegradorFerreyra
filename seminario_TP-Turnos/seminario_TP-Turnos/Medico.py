0676754d10de3086f0f01dbd0fd4dfe7c534eef7
Fernando Ferreyra <fernandoeferreyra01@gmail.com>
2025-09-23 23:35:59 -0300

    Delete seminario_TP-Turnos directory

--- seminario_TP-Turnos/seminario_TP-Turnos/Medico.py
+++
@@ -1,9 +0,0 @@
-from Usuarios import Usuario
-
-class Medico(Usuario):  # Hereda de Usuario
-    def __init__(self, nombre, email, especialidad):
-        super().__init__(nombre, email)  # Llama al constructor de Usuario
-        self._especialidad = especialidad  # Guarda la especialidad
-
-    def get_especialidad(self):
-        return self._especialidad  # Devuelve la especialidad
